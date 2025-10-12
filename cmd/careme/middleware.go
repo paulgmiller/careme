f815edea9923dc8d18f22d7c39a524291f2b9fa7
paul miller <paul.miller@gmail.com>
2025-10-11 17:38:12 -0700

    ignore readiness for now

--- cmd/careme/middleware.go
+++ cmd/careme/middleware.go
@@ -13,6 +13,9 @@ type logger struct {
 func (l *logger) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	start := time.Now()
 	l.Handler.ServeHTTP(w, r)
+	if r.URL.Path == "/ready" {
+		return
+	}
 	slog.Info("request", "method", r.Method, "url", r.URL.Path, "query", r.URL.Query(), "duration", time.Since(start))
 }
 
