3b0b59d5a3d22185a9fe4cc3b60447549d730cfb
paul miller <paul.miller@gmail.com>
2025-11-01 12:36:32 -0700

    improve error logging deparallize

--- internal/recipes/generator.go
+++ internal/recipes/generator.go
@@ -243,27 +243,27 @@ func (g *Generator) GetStaples(ctx context.Context, p *generatorParams) ([]strin
 		return ingredients, nil
 	}
 
-	var errors []error
-	var wg sync.WaitGroup
-	var lock sync.Mutex
-	wg.Add(len(p.Staples))
+	//var errors []error
+	//var wg sync.WaitGroup
+	//var lock sync.Mutex
+	//wg.Add(len(p.Staples))
 	for _, category := range p.Staples {
-		go func(category filter) {
-			defer wg.Done()
+		//func(category filter) {
+		//	defer wg.Done()
 
-			cingredients, err := g.GetIngredients(p.Location.ID, category, 0)
-			lock.Lock()
-			defer lock.Unlock()
-			if err != nil {
-				errors = append(errors, fmt.Errorf("failed to get ingredients: %w", err))
-				return
-			}
-			ingredients = append(ingredients, cingredients...)
-			slog.InfoContext(ctx, "Found ingredients for category", "count", len(cingredients), "category", category.Term, "location", p.Location.ID)
-		}(category)
+		cingredients, err := g.GetIngredients(p.Location.ID, category, 0)
+		//lock.Lock()
+		//defer lock.Unlock()
+		if err != nil {
+			//errors = append(errors, fmt.Errorf("failed to get ingredients: %w", err))
+			return nil, err
+		}
+		ingredients = append(ingredients, cingredients...)
+		slog.InfoContext(ctx, "Found ingredients for category", "count", len(cingredients), "category", category.Term, "location", p.Location.ID)
+		//	}(category)
 	}
 
-	wg.Wait()
+	//wg.Wait()
 
 	if err := g.cache.Set(p.LocationHash(), strings.Join(ingredients, "\n")); err != nil {
 		slog.ErrorContext(ctx, "failed to cache ingredients", "location", p.String(), "error", err)
@@ -292,7 +292,8 @@ func (g *Generator) GetIngredients(location string, f filter, skip int) ([]strin
 	}
 
 	if products.StatusCode() != http.StatusOK {
-		fmt.Printf("Kroger ProductSearchWithResponse returned status: %d\n", products.StatusCode())
+		output, _ := json.Marshal(products.JSON500) //handle other errors?
+		slog.Error("Kroger ProductSearchWithResponse returned error", "term", f.Term, "status", products.StatusCode(), "error", string(output))
 		return nil, fmt.Errorf("got %d code from kroger", products.StatusCode())
 	}
 
