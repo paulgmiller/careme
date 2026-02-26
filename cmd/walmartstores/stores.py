d1bf19a35462b962e1ad0f6e8947849c7bda5f40
paul miller <paul.miller>
2026-02-26 13:08:46 -0800

    clear out python junk

--- cmd/walmartstores/stores.py
+++
@@ -1,9 +0,0 @@
-from wiopy import WalmartIO
-walmart_io = WalmartIO(
-    private_key_version="1",
-    private_key_filename="../../keys/walmart_prod",
-    consumer_id='52dae855-d02f-488b-b179-1df6700d7dcf',
-)
-#data = walmart_io.stores(zip=98007) #lat=29.735577, lon=-95.511747)
-data = walmart_io.product_lookup('33093101')[0]
-print(data)
