5c9f5f1f27ea728824c3e9c06bed8e9a72da6533
Paul Miller <paul.miller@gmail.com>
2026-03-17 17:16:57 -0700

    Double include saved in mail
    Removed redundant loop for filtering recent recipes.

--- internal/mail/mail.go
+++ internal/mail/mail.go
@@ -177,12 +177,7 @@ func (m *mailer) sendEmail(ctx context.Context, user utypes.User) {
 		p.LastRecipes = lo.FilterMap(recent, func(r utypes.Recipe, _ int) (string, bool) {
 			return r.Title, cooked[r.Hash]
 		})
-		for _, last := range recent {
-			if cooked[last.Hash] {
-				continue
-			}
-			p.LastRecipes = append(p.LastRecipes, last.Title)
-		}
+		
 		// can orphan recipes here with crash or shutdown. Params should have a start time
 
 		shoppingList, err = m.generator.GenerateRecipes(ctx, p)
