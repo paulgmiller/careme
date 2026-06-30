11c875c5ccc40929c250630154f8dc9e22dc63ba
paul miller <paul.miller>
2026-06-30 14:41:43 -0700

    don't touch some things

--- internal/recipes/staples.go
+++ internal/recipes/staples.go
@@ -268,6 +268,21 @@ func (s *cachedStaplesService) Watchdog(ctx context.Context) error {
 	return err
 }
 
+// StaplesWatchdogLocations returns the stores checked by the staples watchdog.
+func StaplesWatchdogLocations() []locations.Location {
+	stores := []locations.Location{
+		{ID: "wholefoods_10153", ZipCode: "97209"},
+		{ID: "safeway_490", ZipCode: "86403"},
+		{ID: "70500874", ZipCode: "98101"},
+		{ID: "starmarket_3566", ZipCode: "02108"},
+		{ID: "acmemarkets_806", ZipCode: "19711"},
+		{ID: "publix_1847", ZipCode: "35401"},
+		{ID: "aldi_F219", ZipCode: "40222"},
+		{ID: "heb_540", ZipCode: "77023"},
+	}
+	return stores
+}
+
 func staplesSignatureForLocation(locationID string) string {
 	for _, provider := range defaultIdentityProviders() {
 		if provider.IsID(locationID) {
